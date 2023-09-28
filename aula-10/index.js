84b7093c97561c3ee341be885b1ba9d2395d43a4
LuizDev <LuizDeveloper@gmail.com>
2023-09-28 11:36:35 -0300

    Recursos nativos do navegaor

---
+++ aula-10/index.js
@@ -0,0 +1,17 @@
+let nome = "Luiz";
+
+console.log(nome);
+
+console.error(nome);
+
+console.warn(nome);
+
+// window.alert(nome)
+
+// nome = prompt("insira o seu nome:")
+
+// let confirmacao = confirm("Confirma essa ação?");
+
+// console.log(confirmacao);
+
+alert("Olá mundo!");
