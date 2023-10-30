9cb1c3f85b7dd5ac213b5dcd4eb1cba7c7d43d1b
LuizDev <LuizDeveloper@gmail.com>
2023-10-29 22:56:36 -0300

    IF e Else

---
+++ aula-16/index.js
@@ -0,0 +1,15 @@
+// const idade = prompt("informe sua idade");
+
+// if (idade > 18) {
+//   alert("Você e de maior");
+// } else if (idade > 12) {
+//   alert("Voce e de menor");
+// } else if (idade > 4) {
+//   alert("Voçe e bb");
+// } else {
+//   alert("..");
+// }
+
+const resultado = 6 === 5 ? "verdadeiro" : "false";
+
+console.log(resultado);
