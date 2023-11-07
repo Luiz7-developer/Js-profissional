e7c9cdaeeda2a8c8a0ae6a104c095c189b2a39e6
LuizDev <LuizDeveloper@gmail.com>
2023-11-06 22:55:16 -0300

    testando git

--- ex-Arrays/index.js
+++ ex-Arrays/index.js
@@ -10,6 +10,6 @@ function FraseBomDia(pronome, nome, sobrenome) {
 
 const Pronomes = ["Sra.", "Sr.", "Srta."];
 const Nomes = ["Carla", "Paulo", "Eduarda"];
-const Sobrenomes = ["Nascimento", "Passos", "Lorrayn"];
+const Sobrenomes = ["Nascimento", "Passos", "Lorrayne"];
 
 console.log(FraseBomDia(Pronomes, Nomes, Sobrenomes));
