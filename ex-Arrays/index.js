a9b17cab79e086f8492c15fc05669de53ad0af83
LuizDev <LuizDeveloper@gmail.com>
2023-11-06 22:45:38 -0300

    arrays

--- ex-Arrays/index.js
+++ ex-Arrays/index.js
@@ -2,8 +2,6 @@ function FraseBomDia(pronome, nome, sobrenome) {
   const frases = [];
 
   for (let i = 0; i < nome.length; i++) {
-    // const frase =
-    //   "Olá" + pronome[0] + nome[0] + sobrenome[0] + ", tenha um bom dia!";
     const frase = `Olá ${pronome[i]} ${nome[i]} ${sobrenome[i]}, tenha um Bom dia!!`;
     frases.push(frase);
   }
