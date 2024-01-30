59dcae3061b7e5fd8c3502270fc376b706813df9
LuizDev <LuizDeveloper@gmail.com>
2024-01-30 19:34:30 -0300

    exercicio do while

---
+++ exercicios/exercicio-6/while.js
@@ -0,0 +1,22 @@
+const nome = prompt("Qual o seu nome?");
+let cidades = "";
+let contagem = 0;
+
+let continuar = prompt("Você visitou alguma cidade? (Sim/Não)");
+
+while (continuar === "Sim") {
+  let cidade = prompt("Qual e o nome da cidade visitada?");
+  // += vai contatenar as cidades : cidades = cidades + ...
+  cidades += " - " + cidade + "\n";
+  contagem++;
+  continuar = prompt("Você vistou alguma outra cidade? (Sim/Não)");
+}
+
+alert(
+  "Turista: " +
+    nome +
+    "\nQuantidade de cidades visitadas: " +
+    contagem +
+    "\nCidades visitadas:\n" +
+    cidades
+);
